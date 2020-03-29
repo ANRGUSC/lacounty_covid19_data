49ebad29f428f5c0c42a64d61741695512efbd8f
gowrivs <>
2020-03-29 13:39:36 -0700

    population data format modified

--- scripts/population_parse.py
+++ scripts/population_parse.py
@@ -7,17 +7,21 @@ script_dir = os.path.dirname(__file__)
 rel_path = "../data/population.csv"
 abs_file_path = os.path.join(script_dir, rel_path)
 
+out_file="../data/population.json"
+out_path=os.path.join(script_dir, out_file)
+
 #this dictionaty will hold the parsed data in JSON format
-population_dict=[]
+population_dict={}
 
 #parsing CSV
 with open(abs_file_path, 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',')
     for row in spamreader:
-        population_dict.append([row[0],row[1]])
+        #population_dict.append([row[0],row[1]])
+        population_dict[str(row[0])]=str(row[1])
 
 #writing dictionary to a JSON file
-out_file = open('population.json','w+')
+out_file = open(out_path,'w+')
 json.dump(population_dict, out_file)
 
 
