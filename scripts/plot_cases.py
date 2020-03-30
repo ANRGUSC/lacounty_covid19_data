0e800abb0f0cdede7b5ddc4cf5864fea7bc07c2e
mehrdadkiam <39785823+mehrdadkiam@users.noreply.github.com>
2020-03-29 20:05:20 -0700

    Update plot_cases.py

--- scripts/plot_cases.py
+++ scripts/plot_cases.py
@@ -1,114 +1,134 @@
-# Copyright (c) 2020, Autonomous Networks Research Group. All rights reserved.
-#      contributors: Gowri Ramachandran, Mehrdad Kiamari, Bhaskar Krishnamachari
-#      Read license file in main directory for more details  
-# 
-# This script generates plots of the covid-19 data from LA County 
-
 import json
 import re
 import matplotlib.pyplot as plt
 import numpy as np
-import os
-
-#setting up the file path
-script_dir = os.path.dirname(__file__)
-rel_path = "../data/lacounty_covid.json"
-abs_file_path = os.path.join(script_dir, rel_path)
-
+from matplotlib.colors import hsv_to_rgb
 
 class community:
 	def __init__(self,name,actual_name,Today_date):
 		self.name = name
-		self.actual_name = actual_name # for displaying part of figures
-		self.dic_confirmed = {}
-		self.total_confirmed_so_far = 0
 		self.Today_date = Today_date
-		self.dic_confirmed_cummulative = np.zeros(len(range(16,self.Today_date)),dtype=int) # for cumulative confirmed cases for each day
-		
-		
-	def calculate_total_confirmed_so_far(self):
-		for day in self.dic_confirmed.keys():
-			self.total_confirmed_so_far += self.dic_confirmed[day]
-	# for adding new entry for each community on every day 		
-	def addnumber(self,day, number):
-		self.dic_confirmed[day] = number
-		# increase cumulative confirmed cases here
-		if day == 16:
-			self.dic_confirmed_cummulative[day - 16] = number	
+		self.actual_name = actual_name # for displaying part of figures
+		# cumulative total
+		self.confirmed = np.zeros(len(range(16,self.Today_date)),dtype=int)
+		self.confirmed_daily = np.zeros(len(range(16,self.Today_date)),dtype=int)
+	# for adding new entry for each community on every day 	
+	def check_validity_new_entry(self,day):
+		index = day - 16
+		if index == 0:
+			return True
 		else:
-			self.dic_confirmed_cummulative[day - 16] =  self.dic_confirmed_cummulative[day - 16 - 1] +  number	
-	# for printing purposes only		
-	def print_stat(self):
-		print("name", self.name)
-		for day in self.dic_confirmed.keys():
-			print(day, self.dic_confirmed[day])
+			if self.confirmed[index] >= self.confirmed[index-1]:
+				return True
+			return False		
+	def update_confirmed_cases(self,day):
+		index = day - 16
+		while index != 0:
+			if self.confirmed[index] < self.confirmed[index-1]:
+				self.confirmed[index-1] = self.confirmed[index]
+			index -= 1
+
+	def addnumber(self,day, number):
+		index = day - 16
+		self.confirmed[index] = number
+		status_validity_of_entry = self.check_validity_new_entry(day)
+		if not status_validity_of_entry:
+			self.update_confirmed_cases(day)
+
 	# return the confirmed cases (either daily or cumulative) for each community		
 	def plot_info(self,type_plot):
 		output = np.zeros(len(range(16,self.Today_date)),dtype=int)
 		for index,i in enumerate(list(range(16,self.Today_date))):
 			# for daily
 			if type_plot == 'daily':
-				if i in self.dic_confirmed.keys():
-					output[index] =  self.dic_confirmed[i]
-				else:
-					output[index] = 0
+				# if i in self.dic_confirmed.keys():
+				# 	output[index] =  self.dic_confirmed[i]
+				# else:
+				# 	output[index] = 0
+				output = self.confirmed_daily
 			# for cumulative
 			else:
-				output = self.dic_confirmed_cummulative			
+				output = self.confirmed
 		return output	
 
+def get_population_vec(list_communities):
+	with open('population.json') as json_file_pop:
+		data = json.load(json_file_pop)
+		#print(data)
+		dictionary_population_commnunities ={}
+		for instance in data:
+			dictionary_population_commnunities[instance[0].strip().split('--')[0]] = int(instance[1].strip())
+		#print(dictionary_population_commnunities)
+		#raise NameError('HiThere')
+		output_list = []
+		for communiuty_obj in list_communities:
+			temp = [val for key,val in dictionary_population_commnunities.items() if communiuty_obj.actual_name == key]
+			if temp :
+		 		output_list.append(temp.pop())
+		if len(output_list) == len(list_communities):
+			output = np.asarray(output_list)
+		else:
+			raise NameError('The name of one of communities has NOT been found!')	 		
+		return output
+		
 def main(top_i_comm, type_plot,Today_date):
 	dict_county = {} # dictionary of all community objects
 	list_communities = [] # list of all community objects
-	list_pair = []	
-        #make sure the json file is in the right directory
-	with open(abs_file_path) as json_file:
+	list_pair = []			
+	with open('lacounty_covid.json') as json_file:
 		data = json.load(json_file)
 		for day in sorted([int(k) for k in data.keys()]):
-			for i in range(len(data[str(day)])):
-				actual_name_of_community = 	data[str(day)][i][0].strip()
-				name_of_community = data[str(day)][i][0].strip().lower().replace(' ','')
-				# cleaning city names, removing following prefixes
-				prefixex = ['cityof','losangeles-','unincorporated-']
-				for word in prefixex:
-					name_of_community = name_of_community.replace(word,'') 
-				# cleaning confirmed number, e.g. <1 will be 1
-				confirmed_cases   = data[str(day)][i][0].strip().lower(),re.sub("[^0-9]", "", data[str(day)][i][1].strip())
-				if name_of_community not in dict_county.keys():
-					dict_county[name_of_community] = community(name_of_community,actual_name_of_community,Today_date)
-					list_communities.append(dict_county[name_of_community ])  
-					dict_county[name_of_community].addnumber(day,int(confirmed_cases[1]))
+			if day < Today_date :
+				#print(day, Today_date )
+				for i in range(len(data[str(day)])):
+					actual_name_of_community = 	data[str(day)][i][0].strip()
+					name_of_community = data[str(day)][i][0].strip().lower().replace(' ','')
+					# cleaning city names, removing following prefixes
+					prefixex = ['cityof','losangeles-','unincorporated-']
+					for word in prefixex:
+						name_of_community = name_of_community.replace(word,'') 
+					# cleaning confirmed number, e.g. <1 will be 1
+					confirmed_cases   = data[str(day)][i][0].strip().lower(),re.sub("[^0-9]", "", data[str(day)][i][1].strip())
+					if name_of_community not in dict_county.keys():
+						dict_county[name_of_community] = community(name_of_community,actual_name_of_community,Today_date)
+						list_communities.append(dict_county[name_of_community ])  
+						dict_county[name_of_community].addnumber(day,int(confirmed_cases[1]))
+					else:
+						dict_county[name_of_community].addnumber(day,int(confirmed_cases[1]))
+
+		# get daily cases of all communities because the cumulative is already obtained
+		for communiuty_obj in list_communities:
+			for index in range(len(communiuty_obj.confirmed)):
+				if index == 0:
+					communiuty_obj.confirmed_daily[index] = communiuty_obj.confirmed[index]
 				else:
-					dict_county[name_of_community].addnumber(day,int(confirmed_cases[1]))
+					communiuty_obj.confirmed_daily[index] = communiuty_obj.confirmed[index] - communiuty_obj.confirmed[index-1]	
+		#import pdb; pdb.set_trace()				
 		# find communiuty with highest 
-		for communiuty in dict_county.keys():
-			dict_county[communiuty].calculate_total_confirmed_so_far()
-			#print(dict_county[communiuty].total_confirmed_so_far)
-
+		# for communiuty in dict_county.keys():
+		# 	dict_county[communiuty].calculate_total_confirmed_so_far()
+	
 		#days = list(range(16,Today_date))
-		days = list(range(1,Today_date-16 +1))
-
-		# sort communities in the whole list based on total confirmed cases so far and plot top i communities
-		newlist = sorted(list_communities,key=lambda x: x.total_confirmed_so_far, reverse=True)
+		list_selected_communities = []
+		days = list(range(1,Today_date-16+1))
+		newlist = sorted(list_communities,key=lambda x: x.confirmed[-1], reverse=True)
 		for en,communiuty_obj in enumerate(newlist):
 			if communiuty_obj.name != '-investigatedcases' and communiuty_obj.name !='-underinvestigation' and top_i_comm > 0:
-				#print(en,communiuty_obj.name, communiuty_obj.total_confirmed_so_far)
-				#print(communiuty_obj.dic_confirmed)
-				plt.plot(days, communiuty_obj.plot_info(type_plot),'o--',label = communiuty_obj.actual_name)
-				#print(top_i_comm, communiuty_obj.plot_info(type_plot))
+				# append this city to the list
+				list_selected_communities.append(communiuty_obj)
+				plt.plot(days, communiuty_obj.plot_info(type_plot),'o-',label = communiuty_obj.actual_name)
 				top_i_comm -= 1
 		plt.legend()
 		plt.xlabel('Days since March 16, 2020')
-		plt.ylabel('Number of Confirmed Cases')
-		plt.yscale('log')
+		plt.ylabel('Cases')
 		plt.grid(True)
-		plt.title(type_plot)
+		# plt.yscale('log')
+		#plt.title(type_plot)
 		plt.show()		
-	
-
+		
 if __name__ == "__main__":
 	top_k_community_with_highest_confirmed = 6
 	# Display mode: daily or cumulative
-	display_mode = 'cumulative'
+	display_mode = 'daily'
 	number_of_days_passed_from_16th = 12
 	main(top_k_community_with_highest_confirmed,display_mode, 16 + number_of_days_passed_from_16th)		
