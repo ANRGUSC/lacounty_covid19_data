22fcc80600f7aceee152f61b89131d113a5582a0
gowrivs <gshan.r@gmail.com>
2020-09-12 21:14:14 -0700

    new data and plots added

--- scripts/plot_cases_daily.py
+++ scripts/plot_cases_daily.py
@@ -127,5 +127,5 @@ if __name__ == "__main__":
 	top_k_community_with_highest_confirmed = 6
 	# Display mode: daily or cumulative
 	display_mode = 'daily'
-	number_of_days_passed_from_16th = 179
+	number_of_days_passed_from_16th = 180
 	main(top_k_community_with_highest_confirmed,display_mode, 16 + number_of_days_passed_from_16th)
