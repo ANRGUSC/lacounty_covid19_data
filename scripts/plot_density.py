f19d0f5950b6dd321125a43ed43e8167d8cd225f
gowrivs <gshan.r@gmail.com>
2020-09-17 10:35:19 -0700

    new data and plots added

--- scripts/plot_density.py
+++ scripts/plot_density.py
@@ -165,5 +165,5 @@ if __name__ == "__main__":
 	top_k_community_with_highest_confirmed = 6
 	# Display mode: daily or cumulative
 	display_mode = 'cumulative'
-	number_of_days_passed_from_16th = 184
+	number_of_days_passed_from_16th = 185
 	main(top_k_community_with_highest_confirmed,display_mode, 16 + number_of_days_passed_from_16th)
